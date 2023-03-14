4122d293cef09fa97b2c818cec9e402889df2495
Tomasz Paruzel <tom.krzy.par@gmail.com>
2023-03-14 14:02:08 +0100

    list 2

--- list_1/lista1.sql
+++ list_1/lista1.sql
@@ -6,13 +6,33 @@
 
 4. SELECT FLOOR((EXP(1)-2.71828180)*100000000);
 
-5. 
+5. SELECT FLOOR(0 +RAND()*(7-0));
 
 6. SELECT nazwa,cena FROM produkty;
 
 7. 
 
-8. 
+8. SELECT CONCAT_WS(', ',nazwa,adres,miasto,telefon) AS wizytówki FROM klienci;
 
+9. 
 
+10. 
+
+11. 
+
+12. 
+
+13. 
+
+14. 
+
+15. 
+
+16. 
+
+17. 
+
+18. 
+
+19. 
 
