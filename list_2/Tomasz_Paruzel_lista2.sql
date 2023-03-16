f6187693da56f7e7577b4b5b4d440b5b3b16a7d5
Tomasz Paruzel <tom.krzy.par@gmail.com>
2023-03-16 13:48:58 +0100

    file changes

--- list_2/Tomasz_Paruzel_lista2.sql
+++ list_2/Tomasz_Paruzel_lista2.sql
@@ -1,15 +1,14 @@
-9. SELECT adres FROM klienci 
-   LEFT JOIN zamow ON idk=k_id   
+9. SELECT adres FROM klienci
+   LEFT JOIN zamow ON idk=k_id
    LEFT JOIN detal_zamow ON z_id = idz WHERE p_id = 1
-   ORDER BY REVERSE(nazwa) ASC; 
+   ORDER BY REVERSE(nazwa) ASC;
 
-11. 
+11.
 
 14. SELECT nazwa FROM produkty
     LEFT JOIN detal_zamow ON p_id = idp
     WHERE sztuk IS NULL;
 
-15. SELECT nazwa FROM klienci 
+15. SELECT nazwa FROM klienci
     LEFT JOIN zamow ON idk=k_id WHERE k_id IS NULL;
- 
 
