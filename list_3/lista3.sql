75276732a680fc77a65970828e1de672579c38e1
Tomasz Paruzel <tom.krzy.par@gmail.com>
2023-03-21 14:47:36 +0100

    .

--- list_3/lista3.sql
+++ list_3/lista3.sql
@@ -3,7 +3,13 @@
 7. SELECT nazwa, SUM(sztuk)*cena AS suma_wartosci FROM detal_zamow
    LEFT JOIN produkty ON p_id=idp GROUP BY p_id HAVING suma_wartosci > 7000;
 
-9. SELECT cococo
+9. SELECT DISTINCT nazwa,AVG(sztuk) FROM produkty 
+   LEFT JOIN detal_zamow ON idp=p_id
+   LEFT JOIN zamow ON idz=z_id WHERE DAYOFWEEK(data)=6;
+
+ SELECT nazwa,AVG(sztuk) AS srednia_ilosc_sztuk FROM detal_zamow
+ LEFT JOIN produkty ON idp=p_id
+ LEFT JOIN zamow ON idz=z_id GROUP BY p_id;
 
 14. 
 
