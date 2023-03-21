3ececdaccb213d42b1348a331a167dfa3f56bb71
Tomasz Paruzel <tom.krzy.par@gmail.com>
2023-03-21 11:38:57 +0100

    task 1 and 7 ready, 9 and 14 pending

--- list_3/lista3.sql
+++ list_3/lista3.sql
@@ -1,6 +1,7 @@
-1. 
+1. SELECT SUM(cena*ilosc) FROM produkty;
 
-7. 
+7. SELECT nazwa, SUM(sztuk)*cena AS suma_wartosci FROM detal_zamow
+   LEFT JOIN produkty ON p_id=idp GROUP BY p_id HAVING suma_wartosci > 7000;
 
 9. 
 
